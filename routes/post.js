95dfab0d2aa60c59d29e186e11994330dca7c628
장성호 <jangseongho@jangseonghoui-iMac.local>
2021-04-28 20:51:36 +0900

    S3 commit준비완료

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
 const { Post, Hashtag } = require("../models");
 const { isLoggedIn } = require("./middlewares");
@@ -14,6 +16,12 @@ try {
   console.error("폴더가 없어 생성합니다.");
   fs.mkdirSync("uploads");
 }
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
 
 const upload = multer({
   storage: multer.diskStorage({
